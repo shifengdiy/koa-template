d101ba9dca908e2d4bf0f76e35ce84d253adc0a5
shifeng <shifengdiy@qq.com>
2019-01-23 08:53:07 +0800

    fix: 修复冲突， feat: 添加项目结构，修改route，准备使用子路由和总路由组合的方式

--- app/router.js
+++ app/router.js
@@ -1,15 +1,15 @@
+const KoaRouter = require('koa-router');
+const router = new KoaRouter();
+
+//请求控制器内的模块，根据功能分割的不同文件，方便管理
+const { root } = require('./controller/base.js');
+
+/**
+ * 路由配置表设置，根据请求类型分类
+ */
+router.get('/root', root);
+
 module.exports = function(app) {
-  const KoaRouter = require('koa-router');
-  const router = new KoaRouter();
-
-  //请求控制器内的模块，根据功能分割的不同文件，方便管理
-  const { root } = require('./controller/base.js');
-  
-  /**
-   * 路由配置表设置，根据请求类型分类
-   */
-  router.get('/root', root);
-
   app.use(router.routes());
   app.use(router.allowedMethods());
 }
