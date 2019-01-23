4d4e19bb1574c31e4891341f2d28b54673f225da
shifeng <shifengdiy@qq.com>
2019-01-23 11:01:16 +0800

    添加微信域名验证

--- app/router.js
+++ app/router.js
@@ -4,10 +4,15 @@ const router = new KoaRouter();
 //请求控制器内的模块，根据功能分割的不同文件，方便管理
 const { root } = require('./controller/base.js');
 
+//微信公众号相关接口验证
+const { wechatUrlVerify } = require('./controller/wechat.js');
+
 /**
  * 路由配置表设置，根据请求类型分类
  */
 router.get('/root', root);
+router.get('/', wechatUrlVerify);
+router.get('/wx', wechatUrlVerify)
 
 module.exports = function(app) {
   app.use(router.routes());
