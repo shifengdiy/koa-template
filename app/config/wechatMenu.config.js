e666917ef750b9d684eba5adac09990037cd4656
shifeng <shifengdiy@qq.com>
2019-01-25 10:51:22 +0800

    feat: 修改微信菜单配置

--- app/config/wechatMenu.config.js
+++ app/config/wechatMenu.config.js
@@ -1,24 +1,54 @@
 module.exports = {
   "button": [
     {
-      "name": "故障上报",
+      "name": "市民互动",
       "sub_button": [
         {
-          "type": "scancode_push",
-          "name": "扫一扫",
-          "key": "rselfmenu_0_1"
+          "type": "view",
+          "name": "故障上报",
+          "url": "https://mall.leyoobao.com/H5-MAP/nologo/index.html?sid=1909&source=wx1909"
         },
         {
           "type": "view",
-          "name": "游园",
-          "url": "https://mall.leyoobao.com/H5-MAP/nologo/index.html?sid=1909"
+          "name": "留言建议",
+          "url": "https://mall.leyoobao.com/H5-MAP/nologo/index.html?sid=1909&source=wx1909"
         },
         {
           "type": "view",
-          "name": "测试",
+          "name": "留言列表",
           "url": "http://zhzm.xtdsglc.com/reportHistory"
         }
       ]
-    }
+    },
+    {
+      "type": "view",
+      "name": "自助游园",
+      "url": "https://mall.leyoobao.com/H5-MAP/nologo/index.html?sid=1909&source=wx1909",
+    },
+    {
+      "name": "公园信息",
+      "sub_button": [
+        {
+          "type": "view",
+          "name": "园林简介",
+          "url": "https://mall.leyoobao.com/H5-MAP/nologo/index.html?sid=1909&source=wx1909"
+        },
+        {
+          "type": "view",
+          "name": "机构简介",
+          "url": "https://mall.leyoobao.com/H5-MAP/nologo/index.html?sid=1909&source=wx1909"
+        },
+        {
+          "type": "view",
+          "name": "开关门时间",
+          "url": "http://zhzm.xtdsglc.com/reportHistory"
+        },
+        {
+          "type": "view",
+          "name": "联系方式",
+          "url": "http://zhzm.xtdsglc.com/reportHistory"
+        },
+      ]
+    },
   ]
 }
