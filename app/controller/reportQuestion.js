052a807c635b75ba8fa9b2a879893f7c0a9730d5
shifeng <shifeng@qq.com>
2019-01-27 23:00:12 +0800

    完成webAccessToken定时刷新任务

--- app/controller/reportQuestion.js
+++ app/controller/reportQuestion.js
@@ -8,7 +8,10 @@ const mssql = require('mssql');
 const getCommentView = async function(ctx) {
   const code = ctx.query.code;
   const { getWebAccessTokenByCode } = require('../service/wechatTokenRequest');
-  const { openid } = await getWebAccessTokenByCode(code);
+
+  //设置openid到session，设置acess_token定时刷新
+  getWebAccessTokenByCode(code, ctx);
+
   //返回留言视图页面
   ctx.response.type = 'html';
   ctx.response.body = fs.createReadStream('./app/view/comment.html');
@@ -16,12 +19,24 @@ const getCommentView = async function(ctx) {
 
 //获取留言列表页面
 const getCommentListView = function(ctx) {
+  const code = ctx.query.code;
+  const { getWebAccessTokenByCode } = require('../service/wechatTokenRequest');
+
+  //设置openid到session，设置acess_token定时刷新
+  getWebAccessTokenByCode(code, ctx);
+
   ctx.response.type = 'html';
   ctx.response.body = fs.createReadStream('./app/view/commentList.html');
 } 
 
 //获取故障上报页面
 const getReportQuestionView = function(ctx) {
+  const code = ctx.query.code;
+  const { getWebAccessTokenByCode } = require('../service/wechatTokenRequest');
+
+  //设置openid到session，设置acess_token定时刷新
+  getWebAccessTokenByCode(code, ctx);
+
   ctx.response.type = 'html';
   ctx.response.body = fs.createReadStream('./app/view/reportQuestion.html');
 }
@@ -35,6 +50,14 @@ const getCommentLisData = function(ctx) {
 const postCommentData = async function(ctx) {
   await mssql.connect(dataBase);
   let postData = ctx.request.body;
+  let openId = ctx.session.openId;
+
+  let querySql = `insert`;
+  const queryRes = await mssql.query(querySql);
+  let resObj = {};
+
+  ctx.response.type = 'json';
+  ctx.response.body = JSON.stringify(resObj);
 }
 
 //用户提交故障信息
@@ -43,9 +66,16 @@ const postDeviceDefualtData = function(ctx) {
 }
 
 //获取当前用户上报的故障列表
+const getDeviceDefaultListData = function(ctx){
+
+}
 
 module.exports = {
   getCommentView,
   getCommentListView,
   getReportQuestionView,
+  getCommentLisData,
+  postCommentData,
+  postDeviceDefualtData,
+  getDeviceDefaultListData,
 }
--- app/service/wechatTokenRequest.js
+++ app/service/wechatTokenRequest.js
@@ -1,11 +1,52 @@
 const request = require('request');
 const { appId, appSecret } = require('../config/wechat.config');
 
-const getWebAccessTokenByCode = function(code){
+const getWebAccessTokenByCode = async function(code, ctx){
+  //判断openid是否存在，如果存在说明请求已经存在，web_access_token已经再刷新
+  if(!ctx.session.openId){
+    const reqRes = await getOpenIdByCode(code);
+    ctx.session.openId = reqRes.openid;
+
+    //缓存全局webaccessToken
+    global.webAccessToken = reqRes.access_token;
+    global.webRefreshAccessToken = reqRes.refresh_token;
+    
+    //开始设置web_access_token刷新
+    setInterval(async function() {
+      const intelRes = await refreshWebAccessToken(global.webRefreshAccessToken);
+
+      global.webRefreshAccessToken = intelRes.refresh_token;
+    }, 7100 * 1000);
+  }
+
+}
+
+const getOpenIdByCode = function(code){
   let getTokenUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appId}&secret=${appSecret}&code=${code}&grant_type=authorization_code`;
+
   return new Promise(function(resolve, reject){
     const reqOptions = {
       url: getTokenUrl,
+      method: "GET",
+      json: true,
+    };
+
+    request(reqOptions, function (err, res, body) {
+      if (!err && res.statusCode == 200) {
+        resolve(body);
+      } else {
+        throw (err);
+      }
+    })
+  })
+}
+
+const refreshWebAccessToken = function(refreshToken){
+  let refreshTokenUrl = `https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=${APPID}&grant_type=refresh_token&refresh_token=${refreshToken}`;
+
+  return new Promise(function(resolve, reject){
+    const reqOptions = {
+      url: refreshTokenUrl,
       method: "GET",
       json: true,
     };
@@ -18,4 +59,9 @@ const getWebAccessTokenByCode = function(code){
       }
     })
   })
+
+}
+
+module.exports = {
+  getWebAccessTokenByCode,
 }
