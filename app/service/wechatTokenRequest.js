cde8f86b214e3152f188be2908a477067e036d50
shifeng <shifengdiy@qq.com>
2019-01-28 15:27:34 +0800

    ..

--- app/service/wechatTokenRequest.js
+++ app/service/wechatTokenRequest.js
@@ -3,63 +3,55 @@ const { appId, appSecret } = require('../config/wechat.config');
 
 const getWebAccessTokenByCode = function(code){ 
   return new Promise(function(resolve, reject) {
+    let getTokenUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appId}&secret=${appSecret}&code=${code}&grant_type=authorization_code`;
+
+    const reqOptions = {
+      url: getTokenUrl,
+      method: "GET",
+      json: true,
+    };
     //判断openid是否存在，如果存在说明请求已经存在，web_access_token已经再刷新 
-    getOpenIdByCode(code, resolve);
+    request(reqOptions, function (err, res, body) {
+      console.log(body, '获取openid');
+      console.log(err, body, '请求数据')
+      if (!err && res.statusCode == 200) {
+
+        resolve(body.openid);
+        //缓存全局webaccessToken
+        global.webAccessToken = body.access_token;
+        global.webRefreshAccessToken = body.refresh_token;
+      } else {
+        console.log('抛出异常');
+        //throw (err);
+      }
+    })
 
     //开始设置web_access_token刷新
-    setInterval(async function () {
-      const intelRes = await refreshWebAccessToken(global.webRefreshAccessToken);
-
-      global.webRefreshAccessToken = intelRes.refresh_token;
+    setInterval(  function () {
+      refreshWebAccessToken(global.webRefreshAccessToken);
     }, 7100 * 1000);
   })
 }
 
-const getOpenIdByCode = function (code, resolve){
-  let getTokenUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appId}&secret=${appSecret}&code=${code}&grant_type=authorization_code`; 
-
+const refreshWebAccessToken = function(refreshToken){
+  let refreshTokenUrl = `https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=${APPID}&grant_type=refresh_token&refresh_token=${refreshToken}`;
+ 
   const reqOptions = {
-    url: getTokenUrl,
+    url: refreshTokenUrl,
     method: "GET",
     json: true,
   };
 
   request(reqOptions, function (err, res, body) {
-    console.log(body, '获取openid');
-    console.log(err, body, '请求数据')
     if (!err && res.statusCode == 200) {
-      
-      resolve(body.openid);
+      //缓存全局webaccessToken
+      global.webAccessToken = body.access_token;
+      global.webRefreshAccessToken = body.refresh_token;
     } else {
-      console.log('抛出异常')
-      throw (err);
+      //throw (err);
     }
   })
  
-}
-
-const refreshWebAccessToken = function(refreshToken){
-  let refreshTokenUrl = `https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=${APPID}&grant_type=refresh_token&refresh_token=${refreshToken}`;
-
-  return new Promise(function(resolve, reject){
-    const reqOptions = {
-      url: refreshTokenUrl,
-      method: "GET",
-      json: true,
-    };
-
-    request(reqOptions, function (err, res, body) {
-      if (!err && res.statusCode == 200) {
-        resolve(body);
-
-        //缓存全局webaccessToken
-        global.webAccessToken = body.access_token;
-        global.webRefreshAccessToken = body.refresh_token;
-      } else {
-        throw (err);
-      }
-    })
-  })
 
 }
 
