59aa3c29c1235f90a862a8b4cd41df1dd720516d
shifeng <shifengdiy@qq.com>
2019-01-28 16:21:45 +0800

    ..

--- app/service/wechatTokenRequest.js
+++ app/service/wechatTokenRequest.js
@@ -3,7 +3,7 @@ const { appId, appSecret } = require('../config/wechat.config');
 
 const getWebAccessTokenByCode = function(code){
   console.log(code) 
-  return new Promise(function(resolve, reject) {
+  // return new Promise(function(resolve, reject) {
     let getTokenUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appId}&secret=${appSecret}&code=${code}&grant_type=authorization_code`;
 
     const reqOptions = {
@@ -13,26 +13,26 @@ const getWebAccessTokenByCode = function(code){
     };
     //判断openid是否存在，如果存在说明请求已经存在，web_access_token已经再刷新 
     console.log('web_access_token已经再刷新 ')
-    request(reqOptions, function (err, res, body) {
-      console.log(body, '获取openid');
-      console.log(err, body, '请求数据')
-      if (!err && res.statusCode == 200) {
+  //   request(reqOptions, function (err, res, body) {
+  //     console.log(body, '获取openid');
+  //     console.log(err, body, '请求数据')
+  //     if (!err && res.statusCode == 200) {
 
-        resolve(body.openid);
-        //缓存全局webaccessToken
-        global.webAccessToken = body.access_token;
-        global.webRefreshAccessToken = body.refresh_token;
-      } else {
-        console.log('抛出异常');
-        //throw (err);
-      }
-    })
+  //       resolve(body.openid);
+  //       //缓存全局webaccessToken
+  //       global.webAccessToken = body.access_token;
+  //       global.webRefreshAccessToken = body.refresh_token;
+  //     } else {
+  //       console.log('抛出异常');
+  //       //throw (err);
+  //     }
+  //   })
 
-    //开始设置web_access_token刷新
-    setInterval(  function () {
-      refreshWebAccessToken(global.webRefreshAccessToken);
-    }, 7100 * 1000);
-  })
+  //   //开始设置web_access_token刷新
+  //   setInterval(  function () {
+  //     refreshWebAccessToken(global.webRefreshAccessToken);
+  //   }, 7100 * 1000);
+  // })
 }
 
 const refreshWebAccessToken = function(refreshToken){
